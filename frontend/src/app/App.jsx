259f628595b262961c4037f8cf90b80df70199cd
CreepyMemes <mjouins@gmail.com>
2025-05-04 16:32:51 +0200

    added way to view items on the frontend

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,7 +1,10 @@
 import styles from './App.module.css';
 import Logo from '@components/logo/Logo';
 import Form from '@components/form/Form';
+import Item from '@components/item/Item';
 import { useEffect, useState } from 'react';
+
+const apiUrl = 'http://127.0.0.1:8000/'; // Warning this only works on the host machine
 
 function App() {
   const [items, setItems] = useState([]);
@@ -12,9 +15,9 @@ function App() {
 
   const fetchItems = async () => {
     try {
-      const response = await fetch('http://127.0.0.1:8000/api/items/');
+      const response = await fetch(`${apiUrl}/api/items/`);
       const data = await response.json();
-      console.log(data);
+      setItems(data);
     } catch (error) {
       console.log(error);
     }
@@ -26,6 +29,13 @@ function App() {
       <h1>BarberManager</h1>
       <Form />
       <p className={styles.text}> Testing API CRUD operations</p>
+
+      <h2>Items: </h2>
+      <div className={styles.itemList}>
+        {items.map((item) => (
+          <Item key={item.id} item={item}></Item>
+        ))}
+      </div>
     </>
   );
 }
