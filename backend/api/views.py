e6601f42e01e27578ece374f9f135c6c034fa7e0
CreepyMemes <mjouins@gmail.com>
2025-05-14 07:56:17 +0200

    added basic authentication

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,22 +1,10 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
-from .models import Item
-from .serializers import ItemSerializer
-
-@api_view(['GET'])
-def get_items(request):
-    items = Item.objects.all()
-    serializer = ItemSerializer(items, many=True)
-    return Response(serializer.data)
-
-@api_view(['POST'])
-def create_item(request):
-    serializer = ItemSerializer(data=request.data)
-
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
