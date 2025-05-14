b78fe950313eeee2759c884632c1b8ce72eca188
CreepyMemes <mjouins@gmail.com>
2025-05-14 06:25:21 +0200

    cleared up frontend template, starting from scratch

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,71 +1,7 @@
-import styles from './App.module.css';
-import Logo from '@components/logo/Logo';
-import Form from '@components/form/Form';
-import Item from '@components/item/Item';
-import { useEffect, useState } from 'react';
-
-const apiUrl = 'http://127.0.0.1:8000/'; // Warning this only works on the host machine
+import styles from './App.module.scss';
 
 function App() {
-  const [items, setItems] = useState([]);
-  const [name, setName] = useState('');
-  const [quantity, setQuantity] = useState(0);
-
-  useEffect(() => {
-    fetchItems();
-  }, []);
-
-  const fetchItems = async () => {
-    try {
-      const response = await fetch(`${apiUrl}/api/items/`);
-      const items = await response.json();
-      setItems(items);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const addItem = async () => {
-    const itemData = {
-      name,
-      quantity,
-    };
-
-    try {
-      const response = await fetch(`${apiUrl}/api/items/create/`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(itemData),
-      });
-
-      const newItem = await response.json();
-      setItems((prevItems) => [...prevItems, newItem]);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  return (
-    <>
-      <Logo />
-      <h1>BarberManager</h1>
-      <Form
-        name={name}
-        quantity={quantity}
-        setName={setName}
-        setQuantity={setQuantity}
-        onSubmit={addItem}
-      />
-      <p className={styles.text}> Testing API CRUD operations</p>
-
-      <h2>Items: </h2>
-      <div className={styles.itemList}>
-        {items.map((item) => (
-          <Item key={item.id} item={item}></Item>
-        ))}
-      </div>
-    </>
-  );
+  return <div> BARBER MANAGER</div>;
 }
 
 export default App;
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,29 +9,28 @@ export default defineConfig({
     react(),
     eslint({
       failOnError: true, // Stop build on ESLint errors
-      failOnWarning: false, // (Optional) Stop build on warnings
+      failOnWarning: false, // Don't stop build on warnings
     }),
   ],
+
   resolve: {
     alias: [
       { find: '@app', replacement: path.resolve(__dirname, './src/app') },
       { find: '@components', replacement: path.resolve(__dirname, './src/components') },
-      { find: '@common', replacement: path.resolve(__dirname, './src/common') },
       { find: '@assets', replacement: path.resolve(__dirname, './src/assets') },
-      { find: '@data', replacement: path.resolve(__dirname, './src/data') },
     ],
   },
+
   css: {
     modules: {
       localsConvention: 'camelCaseOnly',
     },
   },
 
-  //In Django, prefix your routes with /api/ (e.g., path('api/hello/', ...)) to keep backend endpoints well-scoped.
   server: {
     host: true,
     watch: {
-      usePolling: true,
+      usePolling: true, // For hot reloading
     },
   },
 });
