fcdce5b5d375a04291cf21859cfc151cbcc6c739
CreepyMemes <mjouins@gmail.com>
2025-05-16 22:23:17 +0200

    changed get user serializer name

--- backend/api/serializers/user.py
+++ backend/api/serializers/user.py
@@ -5,17 +5,10 @@ from rest_framework import serializers
 User = get_user_model()
 
 
-class UserSerializer(serializers.ModelSerializer):
+class GetUserSerializer(serializers.ModelSerializer):
     """
     Get information on currently logged in user
     """
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'role']
-
-
-class BarberInviteSerializer(serializers.Serializer):
-    """
-    Invite a barber: accept only email.
-    """
-    email = serializers.EmailField()
