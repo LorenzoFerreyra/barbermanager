3369cd27ad96c3ed56702fda68bc74eee776ed4e
CreepyMemes <mjouins@gmail.com>
2025-05-16 15:06:36 +0200

    fixed inactive email saying wrong credentials

--- backend/api/auth_backends.py
+++ backend/api/auth_backends.py
@@ -1,26 +1,25 @@
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
+from django.db.models import Q
+
 
 User = get_user_model()
 
-class EmailOrUsernameModelBackend(ModelBackend):
+
+class UsernameOrEmailBackend(ModelBackend):
     """
     Authenticate with either username or email.
     """
     def authenticate(self, request, username=None, password=None, **kwargs):
         if username is None or password is None:
             return None
-
+        
         try:
-            user = User.objects.get(username=username)
+            user = User.objects.get(Q(username=username) | Q(email=username))
         except User.DoesNotExist:
+            return None
 
-            try:
-                user = User.objects.get(email=username)
-            except User.DoesNotExist:
-                return None
-
-        if user.check_password(password) and self.user_can_authenticate(user):
+        if user.check_password(password):
             return user
         
         return None
