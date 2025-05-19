543404aae82067e923be423873dbbc373c038349
CreepyMemes <mjouins@gmail.com>
2025-05-19 11:03:41 +0200

    added django-extensions for generating a model diagram to aid development

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework_simplejwt.token_blacklist',
     'rest_framework',
+    'django_extensions',
     'corsheaders',
     'api',
 ]
