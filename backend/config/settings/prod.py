a62bc4da18f0cb31d6fd9bfdb08276f34ab22185
CreepyMemes <mjouins@gmail.com>
2025-05-21 00:14:45 +0200

    fixed ling again im blind..

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -1,7 +1,7 @@
 from .base import *
 
 DEBUG = False
-FRONTEND_URL = 'http://barbermanager.creepymemes.org'
+FRONTEND_URL = 'http://barbermanager.creepymemes.duckdns.org'
 REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = (
     'rest_framework.renderers.JSONRenderer',
 )
