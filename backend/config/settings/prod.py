554b24f29262fd8d46cb1d2b58637e0f2bb709c0
CreepyMemes <mjouins@gmail.com>
2025-05-21 10:42:24 +0200

    updated domain

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -1,7 +1,7 @@
 from .base import *
 
 DEBUG = False
-FRONTEND_URL = 'http://barbermanager.creepymemes.duckdns.org'
+FRONTEND_URL = os.environ['APP_DOMAIN']
 REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = (
     'rest_framework.renderers.JSONRenderer',
 )
