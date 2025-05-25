fe48e75e5a18ae438181399be6d87b056660c42b
CreepyMemes <mjouins@gmail.com>
2025-05-25 12:40:53 +0200

    added hsts and ssl security stuff

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -5,3 +5,7 @@ FRONTEND_URL = os.environ['FRONTEND_URL']
 REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = (
     'rest_framework.renderers.JSONRenderer',
 )
+SECURE_HSTS_SECONDS = 31536000  # 1 year, match your nginx max-age
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
