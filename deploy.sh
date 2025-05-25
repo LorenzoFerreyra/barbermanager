c01a930c00af9c21d4c9b1c38cc302cc21e03d12
CreepyMemes <mjouins@gmail.com>
2025-05-25 19:24:52 +0200

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ git pull
 echo "Starting containers..."
 docker compose -f docker-compose.prod.yml up -d --build
 
-echo "Reloading Caddy..."
-sudo caddy reload --config /etc/caddy/Caddyfile
+echo "Reloading Nginx..."
+sudo nginx -t && sudo nginx -s reload
