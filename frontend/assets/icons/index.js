4ee1496aa6993076b9f0417dad840933bcc94fef
CreepyMemes <mjouinss@gmail.com>
2025-06-28 13:49:18 +0200

    added icons

--- frontend/assets/icons/index.js
+++ frontend/assets/icons/index.js
@@ -1,9 +1,13 @@
 import barbermanager from './barbermanager.svg';
 import github from './github.svg';
+import dashboard from './dashboard.svg';
+import appointment from './appointment.svg';
 
 const icons = {
   barbermanager,
   github,
+  dashboard,
+  appointment,
 };
 
 export default icons;
