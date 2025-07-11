1fdf34d3a15923bdd4161c9f818ab8709fc8a5f6
CreepyMemes <mjouinss@gmail.com>
2025-07-11 20:01:55 +0200

    - added reusable stylized tag component
    - updated new color variables to support it

---
+++ frontend/src/components/common/Tag/Tag.jsx
@@ -0,0 +1,16 @@
+import styles from './Tag.module.scss';
+
+const COLOR_MAP = {
+  green: styles.green,
+  yellow: styles.yellow,
+  red: styles.red,
+};
+
+function Tag({ className, color, children }) {
+  // Get all style classes into a string
+  const computedClassName = [className, styles.tag, COLOR_MAP[color]].join(' ');
+
+  return <span className={computedClassName}>{children}</span>;
+}
+
+export default Tag;
