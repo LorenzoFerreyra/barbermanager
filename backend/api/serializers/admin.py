f7b4cf411e62b45e1dfd58c9e4c71866d8a1dfee
CreepyMemes <mjouinss@gmail.com>
2025-07-15 11:59:12 +0200

    - changed temporary barber usename to be shorter

--- backend/api/serializers/admin.py
+++ backend/api/serializers/admin.py
@@ -66,7 +66,7 @@ class InviteBarberSerializer(EmailValidationMixin, serializers.Serializer):
     def create(self, validated_data):
         barber = Barber(
             email=validated_data['email'],
-            username= f'barber_{uuid.uuid4()}', # Generate random username placeholder
+            username=f'b_{str(uuid.uuid4())[:8]}', # first 8 chars of UUID (e.g. 'b_d3a7f601')
             is_active=False
         )
         barber.set_unusable_password()
