d7553a2cfaee86eda2f38fb557b8ae1bf8f39a1a
CreepyMemes <mjouinss@gmail.com>
2025-07-26 10:28:41 +0200

    - updated barber services page to use new Modal component for all popups
    - deleted redundant popups in favor of using Modal
    
    - removed redundant div wrapper for pagination, and passed custom classname as we enhanced it before

--- frontend/src/pages/barber/BarberServices/BarberServices.jsx
+++ frontend/src/pages/barber/BarberServices/BarberServices.jsx
@@ -1,15 +1,14 @@
 import { useEffect, useState, useCallback } from 'react';
 import { useAuth } from '@hooks/useAuth';
+import { cleanPayload, isAnyFieldSet } from '@utils/utils';
 import styles from './BarberServices.module.scss';
 import api from '@api';
-
-import CreateServicePopup from './CreateServicePopup/CreateServicePopup';
-import DeleteServicePopup from './DeleteServicePopup/DeleteServicePopup';
-import UpdateServicePopup from './UpdateServicePopup/UpdateServicePopup';
 
 import Icon from '@components/common/Icon/Icon';
 import Pagination from '@components/common/Pagination/Pagination';
 import Button from '@components/common/Button/Button';
+import Modal from '@components/common/Modal/Modal';
+import Input from '@components/common/Input/Input';
 import Spinner from '@components/common/Spinner/Spinner';
 
 function BarberServices() {
@@ -89,143 +88,200 @@ function BarberServices() {
 
   return (
     <>
-      <div className={styles.barberServices}>
-        <Pagination
-          icon="service"
-          label="Services"
-          itemsPerPage={5}
-          loading={isLoading}
-          emptyMessage="No services offered." //
-        >
-          <Pagination.Action>
-            <div className={styles.action}>
-              <Button
-                className={styles.refreshBtn}
-                type="button"
-                color="primary"
-                size="md"
-                onClick={fetchServices}
-                disabled={isLoading}
-              >
-                <span className={styles.line}>
-                  {isLoading ? (
-                    <>
-                      <Spinner size={'sm'} /> Refreshing...
-                    </>
-                  ) : (
-                    <>
-                      <Icon name="refresh" size="ty" /> Refresh services
-                    </>
-                  )}
-                </span>
-              </Button>
+      {/* Offered Services Pagination */}
+      <Pagination
+        className={styles.barberServices}
+        icon="service"
+        label="Services"
+        itemsPerPage={5}
+        loading={isLoading}
+        emptyMessage="No services offered." //
+      >
+        <Pagination.Action>
+          <div className={styles.action}>
+            <Button
+              className={styles.refreshBtn}
+              type="button"
+              color="primary"
+              size="md"
+              onClick={fetchServices}
+              disabled={isLoading}
+            >
+              <span className={styles.line}>
+                {isLoading ? (
+                  <>
+                    <Spinner size={'sm'} /> Refreshing...
+                  </>
+                ) : (
+                  <>
+                    <Icon name="refresh" size="ty" /> Refresh services
+                  </>
+                )}
+              </span>
+            </Button>
 
-              <Button
-                className={styles.actionBtn}
-                type="button"
-                color="primary"
-                size="md"
-                onClick={openCreatePopup} //
-              >
-                <Icon name="plus" size="ty" />
-                <span>Create service</span>
-              </Button>
-            </div>
-          </Pagination.Action>
+            <Button
+              className={styles.actionBtn}
+              type="button"
+              color="primary"
+              size="md"
+              onClick={openCreatePopup} //
+            >
+              <Icon name="plus" size="ty" />
+              <span>Create service</span>
+            </Button>
+          </div>
+        </Pagination.Action>
 
-          {/* Table headers */}
+        {/* Table headers */}
+        <Pagination.Column>
+          <div className={styles.tableTitle}>
+            <Icon name="scissors" size="ty" black />
+            <span className={styles.tableTitleName}>Name</span>
+          </div>
+        </Pagination.Column>
 
-          <Pagination.Column>
-            <div className={styles.tableTitle}>
-              <Icon name="scissors" size="ty" black />
-              <span className={styles.tableTitleName}>Name</span>
-            </div>
-          </Pagination.Column>
+        <Pagination.Column>
+          <div className={styles.tableTitle}>
+            <Icon name="revenue" size="ty" black />
+            <span className={styles.tableTitleName}>Price</span>
+          </div>
+        </Pagination.Column>
 
-          <Pagination.Column>
-            <div className={styles.tableTitle}>
-              <Icon name="revenue" size="ty" black />
-              <span className={styles.tableTitleName}>Price</span>
-            </div>
-          </Pagination.Column>
+        <Pagination.Column>
+          <div className={styles.tableTitle}>
+            <Icon name="id" size="ty" black />
+            <span className={styles.tableTitleName}>Service ID</span>
+          </div>
+        </Pagination.Column>
 
-          <Pagination.Column>
-            <div className={styles.tableTitle}>
-              <Icon name="id" size="ty" black />
-              <span className={styles.tableTitleName}>Service ID</span>
-            </div>
-          </Pagination.Column>
+        <Pagination.Column>
+          <div className={styles.tableTitle}>
+            <Icon name="dial" size="ty" black />
+            <span className={styles.tableTitleName}>Actions</span>
+          </div>
+        </Pagination.Column>
 
-          <Pagination.Column>
-            <div className={styles.tableTitle}>
-              <Icon name="dial" size="ty" black />
-              <span className={styles.tableTitleName}>Actions</span>
-            </div>
-          </Pagination.Column>
+        {/* Table rows */}
+        {services.map((service) => (
+          <Pagination.Row key={service.id}>
+            <Pagination.Cell>
+              <span className={styles.serviceName}>{service.name}</span>
+            </Pagination.Cell>
 
-          {/* Table rows */}
-          {services.map((service) => (
-            <Pagination.Row key={service.id}>
-              <Pagination.Cell>
-                <span className={styles.serviceName}>{service.name}</span>
-              </Pagination.Cell>
+            <Pagination.Cell>
+              <span className={styles.servicePrice}>${service.price}</span>
+            </Pagination.Cell>
 
-              <Pagination.Cell>
-                <span className={styles.servicePrice}>${service.price}</span>
-              </Pagination.Cell>
+            <Pagination.Cell>
+              <div className={styles.serviceId}>
+                <span className={styles.hash}># </span>
+                <span className={styles.id}>{service.id} </span>
+              </div>
+            </Pagination.Cell>
 
-              <Pagination.Cell>
-                <div className={styles.serviceId}>
-                  <span className={styles.hash}># </span>
-                  <span className={styles.id}>{service.id} </span>
-                </div>
-              </Pagination.Cell>
+            <Pagination.Cell>
+              <div className={styles.actions}>
+                <Button
+                  type="button"
+                  size="sm"
+                  color="animated"
+                  onClick={() => openUpdatePopup(service)} //
+                >
+                  <Icon name="pen" size="ty" black />
+                </Button>
 
-              <Pagination.Cell>
-                <div className={styles.actions}>
-                  <Button
-                    type="button"
-                    size="sm"
-                    color="animated"
-                    onClick={() => openUpdatePopup(service)} //
-                  >
-                    <Icon name="pen" size="ty" black />
-                  </Button>
+                <Button
+                  type="button"
+                  size="sm"
+                  color="animated"
+                  onClick={() => openDeletePopup(service)} //
+                >
+                  <Icon name="trash" size="ty" black />
+                </Button>
+              </div>
+            </Pagination.Cell>
+          </Pagination.Row>
+        ))}
+      </Pagination>
 
-                  <Button
-                    type="button"
-                    size="sm"
-                    color="animated"
-                    onClick={() => openDeletePopup(service)} //
-                  >
-                    <Icon name="trash" size="ty" black />
-                  </Button>
-                </div>
-              </Pagination.Cell>
-            </Pagination.Row>
-          ))}
-        </Pagination>
-      </div>
-
-      <CreateServicePopup
+      {/* Create Service Modal */}
+      <Modal
         open={createPopup}
+        fields={{ name: '', price: '' }}
+        action={{ submit: 'Create', loading: 'Creating...' }}
+        onSubmit={handleCreateService}
         onClose={closeCreatePopup}
-        onCreate={handleCreateService} //
-      />
+      >
+        <Modal.Title icon="id">Create Service</Modal.Title>
+        <Modal.Description>Enter the service&apos;s name and price for you newly offered service.</Modal.Description>
 
-      <DeleteServicePopup
+        <Input
+          label="Service name"
+          type="text"
+          name="name"
+          required
+          placeholder="Haircut"
+          size="md" //
+        />
+
+        <Input
+          label="Service Price"
+          type="number"
+          min="1"
+          step="any"
+          name="price"
+          required
+          placeholder="25.99"
+          size="md" //
+        />
+      </Modal>
+
+      {/* Delete Service Modal */}
+      <Modal
         open={deletePopup.open}
+        action={{ submit: 'Delete', loading: 'Deleting...' }}
+        onSubmit={() => handleDeleteService(deletePopup.service?.id)}
         onClose={closeDeletePopup}
-        onDelete={handleDeleteService}
-        service={deletePopup.service} //
-      />
+      >
+        <Modal.Title icon="warning">Delete Service</Modal.Title>
+        <Modal.Description>
+          Are you sure you want to delete <strong>{deletePopup.service?.name}</strong> ? This action cannot be undone.
+        </Modal.Description>
+      </Modal>
 
-      <UpdateServicePopup
+      {/* Update Service Modal */}
+      <Modal
         open={updatePopup.open}
+        fields={{ name: '', price: '' }}
+        action={{ submit: 'Update', loading: 'Updating...' }}
+        onValidate={(payload) => isAnyFieldSet(payload, 'Provide a new name, price, or both to update the service.')}
+        onSubmit={(payload) => handleUpdateService(updatePopup.service?.id, cleanPayload(payload))}
         onClose={closeUpdatePopup}
-        onUpdate={handleUpdateService}
-        service={updatePopup.service} //
-      />
+      >
+        <Modal.Title icon="pen">Update Service</Modal.Title>
+        <Modal.Description>
+          Enter new values to update the service:{' '}
+          <strong>{updatePopup.service?.name} This action cannot be undone.</strong>
+        </Modal.Description>
+
+        <Input
+          label="Service name"
+          type="text"
+          name="name"
+          placeholder={updatePopup.service?.name}
+          size="md" //
+        />
+        <Input
+          label="Service Price"
+          type="number"
+          min="1"
+          step="any"
+          name="price"
+          placeholder={updatePopup.service?.price}
+          size="md" //
+        />
+      </Modal>
     </>
   );
 }
