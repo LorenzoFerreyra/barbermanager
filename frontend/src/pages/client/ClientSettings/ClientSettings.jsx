ebb77b7b59ed005c4d3963a2fc8ca263f3315ee6
CreepyMemes <mjouinss@gmail.com>
2025-07-26 11:11:18 +0200

    - updated settings pages to use new Modal component
    - deleted redundant popup components to reduce code repetition, in favour of using Modal
    
    - fixed picture typo
    
    - changed settings page to use margin instead of padding

--- frontend/src/pages/client/ClientSettings/ClientSettings.jsx
+++ frontend/src/pages/client/ClientSettings/ClientSettings.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState, useCallback } from 'react';
 import { useAuth } from '@hooks/useAuth';
+import { isAnyFieldSet } from '@utils/utils';
 import styles from './ClientSettings.module.scss';
 import api from '@api';
 
@@ -8,13 +9,10 @@ import Form from '@components/common/Form/Form';
 import Input from '@components/common/Input/Input';
 import Icon from '@components/common/Icon/Icon';
 import Button from '@components/common/Button/Button';
+import Modal from '@components/common/Modal/Modal';
 import ProfileImage from '@components/ui/ProfileImage/ProfileImage';
 import Spinner from '@components/common/Spinner/Spinner';
 import Error from '@components/common/Error/Error';
-
-import UploadPicturePopup from '@components/ui/UploadPicturePopup/UploadPicturePopup';
-import DeletePicturePopup from '@components/ui/DeletePicturePopup/DeletePicturePopup';
-import DeleteProfilePopup from '@components/ui/DeleteProfilePopup/DeleteProfilePopup';
 
 function ClientSettings() {
   const { profile, setProfile, logout } = useAuth();
@@ -23,8 +21,8 @@ function ClientSettings() {
   const [isUpdatingProfile, setIsUpdatingProfile] = useState(false); // Used to disable the update profile button
 
   // Popup states
-  const [uploadPicurePopup, setUploadPicurePopup] = useState(false);
-  const [deletePicurePopup, setDeletePicurePopup] = useState(false);
+  const [uploadPicturePopup, setUploadPicturePopup] = useState(false);
+  const [deletePicturePopup, setDeletePicturePopup] = useState(false);
   const [deleteProfilePopup, setDeleteProfilePopup] = useState(false);
 
   /**
@@ -52,12 +50,12 @@ function ClientSettings() {
   if (isLoading) return <Spinner />;
 
   // Upload picture popup state handlers
-  const openUploadPicurePopup = () => setUploadPicurePopup(true);
-  const closeUploadPicurePopup = () => setUploadPicurePopup(false);
+  const openUploadPicturePopup = () => setUploadPicturePopup(true);
+  const closeUploadPicturePopup = () => setUploadPicturePopup(false);
 
   // Delete picture popup state handlers
-  const openDeletePicurePopup = () => setDeletePicurePopup(true);
-  const closeDeletePicurePopup = () => setDeletePicurePopup(false);
+  const openDeletePicturePopup = () => setDeletePicturePopup(true);
+  const closeDeletePicturePopup = () => setDeletePicturePopup(false);
 
   // Delete profile popup state handlers
   const openDeleteProfilePopup = () => setDeleteProfilePopup(true);
@@ -66,18 +64,18 @@ function ClientSettings() {
   /**
    * Handles uploading a new profile picture
    */
-  const handleUploadPicure = async (file) => {
+  const handleUploadPicture = async (file) => {
     await api.image.uploadProfileImage(file);
-    closeUploadPicurePopup();
+    closeUploadPicturePopup();
     await fetchProfile();
   };
 
   /**
    * Handles deleting a new profile picture
    */
-  const handleDeletePicure = async () => {
+  const handleDeletePicture = async () => {
     await api.image.deleteProfileImage();
-    closeDeletePicurePopup();
+    closeDeletePicturePopup();
     await fetchProfile();
   };
 
@@ -129,6 +127,7 @@ function ClientSettings() {
   return (
     <>
       <div className={styles.clientSettings}>
+        {/* Profile Update Card */}
         <StatCard icon="pen" label="Update Profile">
           {/* Profile Picture Management */}
           <section className={styles.profileImageSection}>
@@ -140,7 +139,7 @@ function ClientSettings() {
                 type="button"
                 color="primary"
                 size="md"
-                onClick={openUploadPicurePopup} //
+                onClick={openUploadPicturePopup} //
               >
                 <Icon name="plus" size="ty" />
                 <span>Upload picture</span>
@@ -152,7 +151,7 @@ function ClientSettings() {
                 color="translight"
                 autoIconInvert
                 size="md"
-                onClick={openDeletePicurePopup} //
+                onClick={openDeletePicturePopup} //
               >
                 <Icon name="trash" size="ty" black />
                 <span>Delete picture</span>
@@ -230,8 +229,9 @@ function ClientSettings() {
           </section>
         </StatCard>
 
-        {/* Profile Deletion Management */}
+        {/* Profile Delete Card */}
         <StatCard icon="trash" label="Delete Profile">
+          {/* Profile Deletion Management */}
           <section className={styles.deleteProfileSection}>
             <Button
               className={styles.actionBtn}
@@ -248,23 +248,52 @@ function ClientSettings() {
         </StatCard>
       </div>
 
-      <UploadPicturePopup
-        open={uploadPicurePopup}
-        onClose={closeUploadPicurePopup}
-        onUpload={handleUploadPicure} //
-      />
+      {/* Upload Picture Modal */}
+      <Modal
+        open={uploadPicturePopup}
+        fields={{ profile_image: null }}
+        action={{ submit: 'Upload', loading: 'Uploading...' }}
+        onValidate={(payload) => isAnyFieldSet(payload, 'Please select an image to upload.')}
+        onSubmit={({ profile_image }) => handleUploadPicture(profile_image)}
+        onClose={closeUploadPicturePopup}
+      >
+        <Modal.Title icon="image">Upload Picture</Modal.Title>
+        <Modal.Description>Select a profile image to upload.</Modal.Description>
 
-      <DeletePicturePopup
-        open={deletePicurePopup}
-        onClose={closeDeletePicurePopup}
-        onDelete={handleDeletePicure} //
-      />
+        <Input
+          label="Profile Picture"
+          name="profile_image"
+          type="file"
+          accept="image/*"
+          placeholder="Choose an image"
+        />
+      </Modal>
 
-      <DeleteProfilePopup
+      {/* Delete Picture Modal */}
+      <Modal
+        open={deletePicturePopup}
+        action={{ submit: 'Delete', loading: 'Deleting...' }}
+        onSubmit={handleDeletePicture}
+        onClose={closeDeletePicturePopup}
+      >
+        <Modal.Title icon="warning">Delete Picture</Modal.Title>
+        <Modal.Description>
+          Are you sure you want to delete your profile picture? This action cannot be undone.
+        </Modal.Description>
+      </Modal>
+
+      {/* Delete Profile Modal */}
+      <Modal
         open={deleteProfilePopup}
+        action={{ submit: 'Delete', loading: 'Deleting...' }}
+        onSubmit={handleDeleteProfile}
         onClose={closeDeleteProfilePopup}
-        onDelete={handleDeleteProfile} //
-      />
+      >
+        <Modal.Title icon="warning">Delete Profile</Modal.Title>
+        <Modal.Description>
+          Are you sure you want to delete your profile? This action cannot be undone.
+        </Modal.Description>
+      </Modal>
     </>
   );
 }
