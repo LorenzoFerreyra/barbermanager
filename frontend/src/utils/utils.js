557d579e20899ad8fb8270e5b35a7714efd46e85
CreepyMemes <mjouinss@gmail.com>
2025-07-26 10:12:27 +0200

    - added reusable payload cleaner function, removes empty fields from payload
    - added reusable field checker function, returns true if at least one field in the payload is set

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -15,3 +15,31 @@ export function camelizeStyle(prefix, value) {
 
   return prefix + upper.charAt(0).toUpperCase() + upper.slice(1);
 }
+
+/**
+ * Removes fields that are: undefined, null, empty string, or string with only whitespace, from a passed payload
+ * Trims string fields from spaces
+ */
+export function cleanPayload(payload) {
+  return Object.entries(payload).reduce((acc, [key, value]) => {
+    if (value === undefined || value === null || (typeof value === 'string' && value.trim() === '')) {
+      return acc;
+    }
+
+    acc[key] = typeof value === 'string' ? value.trim() : value;
+    return acc;
+  }, {});
+}
+
+/**
+ * Returns `true` if at least one field in payload has a non-empty value.
+ * Optionally, pass a custom error message as second argument for errors.
+ */
+export function isAnyFieldSet(payload, errorMsg = 'Provide at least one value to update.') {
+  for (const value of Object.values(payload)) {
+    if (value !== undefined && value !== null && (typeof value !== 'string' || value.trim() !== '')) {
+      return true;
+    }
+  }
+  return errorMsg;
+}
